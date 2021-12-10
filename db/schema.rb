528c284bdf07db2c23346975ff26045c53947353
yyoshiokasfdc <yoshiokay1998@gmail.com>
2021-12-10 08:05:11 +0000

    submit assignment

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_050428) do
+ActiveRecord::Schema.define(version: 2021_12_10_080048) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
