b80f5632e7a66e69c59cb69c1014251a26087ba0
yyoshiokasfdc <yoshiokay1998@gmail.com>
2021-12-10 13:42:43 +0000

    add pagination

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    @pagy, @tasks = pagy(Task.order(id: :desc), items: 3)
   end
   
   def show
