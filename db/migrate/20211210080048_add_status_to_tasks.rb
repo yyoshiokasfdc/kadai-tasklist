1da34cd679515f2a9cac02f93cf33147b3b7c63c
yyoshiokasfdc <yoshiokay1998@gmail.com>
2021-12-13 08:42:20 +0000

    user

--- db/migrate/20211210080048_add_status_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToTasks < ActiveRecord::Migration[6.1]
-  def change
-    add_column :tasks, :status, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_061043) do
+ActiveRecord::Schema.define(version: 2021_12_13_074212) do
 
-  create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
+  create_table "tasklists", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.string "status"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_tasks_on_user_id"
+    t.index ["user_id"], name: "index_tasklists_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
@@ -29,5 +29,5 @@ ActiveRecord::Schema.define(version: 2021_12_12_061043) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "tasks", "users"
+  add_foreign_key "tasklists", "users"
 end
