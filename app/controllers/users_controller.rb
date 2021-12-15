9ab5ac853ff1c94a1e9c853ba15ed5be6e69b2c6
yyoshiokasfdc <yoshiokay1998@gmail.com>
2021-12-15 02:29:00 +0000

    submit assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,4 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
-  
-  def index
-    @pagy, @users = pagy(User.order(id: :desc), items: 25)
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @pagy, @tasks = pagy(@user.tasks.order(id: :desc))
-    counts(@user)
-  end
 
   def new
     @user = User.new
